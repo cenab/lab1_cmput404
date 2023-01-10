5ab902a4f2cdbf97d90f563a7691a20391cbdef1
cenab <40250311+cenab@users.noreply.github.com>
2023-01-09 23:20:18 -0700

    recursive request

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import requests
+
+## version number of requests library
 print("Version number of requests: " + requests.__version__)
+
+## google request printed
 print(requests.get("http://google.com/").text)
 
+
+##  github repo prints itselves
+print(requests.get("https://raw.githubusercontent.com/cenab/lab1_cmput404/main/run.py").text)
 
 
 
